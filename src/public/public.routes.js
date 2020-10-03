fb930a6ca9c8e3cf947d5ed9b6b4b567abb9ab0a
shubhamkhaitan <65844445+shubhamkhaitan@users.noreply.github.com>
2020-10-03 16:01:36 +0530

    Update public.routes.js

--- src/public/public.routes.js
+++ src/public/public.routes.js
@@ -40,6 +40,23 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html',
+      controller: 'SignupController',
+      controllerAs: 'signupCtrl'
+    })
+    .state('public.myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/my-info/my-info.html',
+      controller: 'MyInfoController',
+      controllerAs: 'myInfoCtrl',
+      resolve: {
+        userInfo: ['MyInfoService', function(MyInfoService) {
+          return MyInfoService.getMyinfo();
+        }]
+      }
     });
 }
 })();
