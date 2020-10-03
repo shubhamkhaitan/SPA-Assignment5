95381da005e2b71d5fb24b50e79c2062b131788a
shubhamkhaitan <65844445+shubhamkhaitan@users.noreply.github.com>
2020-10-03 16:11:46 +0530

    Add files via upload

---
+++ src/public/my-info/my-info.controller.js
@@ -0,0 +1,14 @@
+(function () {
+    "use strict";
+
+    angular.module('public')
+    .controller('MyInfoController', MyInfoController);
+
+    MyInfoController.$inject = ['userInfo'];
+    function MyInfoController(userInfo) {
+        var ctrl = this;
+        ctrl.userInfo = userInfo;
+        ctrl.saved = !angular.equals(userInfo, {});
+    }
+
+})();
